64332886059cbf3426c6bf7b9ebe6cf3d15fa5d9
MarekOcz <kitty29@talktalk.net>
2025-08-23 13:22:52 +0100

    Began on 2D blocktiling

--- hintfile-1D.cuh
+++ hintfile-1D.cuh
@@ -4,6 +4,12 @@
 
 #define OneDimensional 1
 
+enum class memory_location: uint8_t {
+	REGISTERS = 0,
+	SHARED_MEM = 1,
+	GLOBAL_MEM = 2
+};
+
 constexpr uint32_t SQUARED(const uint32_t val) {
 	return val * val;
 }
--- kernel-3-1D.cuh
+++ kernel-3-1D.cuh
@@ -2,51 +2,32 @@
 #include "hintfile-1D.cuh"
 
 
-template <uint32_t M, uint32_t N, uint32_t K, uint32_t BLOCKSIZE, uint32_t TM = 8, bool load_into_registers = true>
-__global__ void SGEMM(const float* __restrict A, const float* __restrict B, float* const __restrict C, const float alpha = 1.0f, const float beta = 0.0f) {
+template <uint32_t M, uint32_t N, uint32_t K, 
+		  uint32_t BLOCKSIZE_K, uint32_t BLOCKSIZE_M, uint32_t BLOCKSIZE_N = BLOCKSIZE_M, 
+		  uint32_t TM = 8, uint32_t TN = TM, memory_location LOAD_INTO = memory_location::REGISTERS>
+__global__ void SGEMM(const float* __restrict A, const float* __restrict B, float* __restrict C, const float alpha = 1.0f, const float beta = 0.0f) {
 	
-	constexpr uint32_t tileBlockSize = BLOCKSIZE * TM;
+	// Evaluating constexprs. These are optimised at compile time and don't use up a register
+
+	constexpr uint32_t tileBlockLength_M = BLOCKSIZE_M * TM;	// Tile size in the M dimension
+	constexpr uint32_t tileBlockLength_N = BLOCKSIZE_N * TN;	// Tile size in the N dimension
+	constexpr uint32_t tileBlockLength_K = BLOCKSIZE_K;		// Tile size in the K dimension
+
+	constexpr uint32_t numResultsBlocktile = BLOCKSIZE_M * BLOCKSIZE_N;
+
+	constexpr uint32_t numThreadsPerBlock_A = BLOCKSIZE_M * BLOCKSIZE_K;
+	constexpr uint32_t numThreadsPerBlock_B = BLOCKSIZE_K * BLOCKSIZE_N;	
 	
-	uint32_t cCol = BLOCKSIZE * blockIdx.y + (threadIdx.x / BLOCKSIZE);
-	uint32_t cRow = BLOCKSIZE * blockIdx.x + (threadIdx.x % BLOCKSIZE);
-
-	const uint32_t threadIdx_X = threadIdx.x % BLOCKSIZE;
-	const uint32_t threadIdx_Y = threadIdx.x / BLOCKSIZE;
-
-	__shared__ float AS[SQUARED(tileBlockSize)]; // Contains TM*TM elements of A for each thread
-	__shared__ float BS[SQUARED(tileBlockSize)]; // Contains TM*TM elements of B for each thread
-
-	A += tileBlockSize * blockIdx.x;
-	B += tileBlockSize * K * blockIdx.y;
-
-	// May disable if register spills occur in Nsight
-	if constexpr (load_into_registers) {
-		float cReg[SQUARED(TM)];
-	}
-
-	
-	if constexpr (M == N && N == K && M % tileBlockSize == 0) {	// simplest case - 2 square matrices that fit all the blocks perfectly
-		// Iterating the blocks across the columns of A and rows of B
-		for (uint32_t k = 0; k < K; k += tileBlockSize) {
-			// Loading the global memory into smem
-			#pragma unroll
-			for (uint32_t offset_x = 0; offset_x < tileBlockSize; offset_x+=BLOCKSIZE) {
-				for (uint32_t offset_y = 0; offset_y < tileBlockSize; offset_y+=BLOCKSIZE) {
-					
-					uint32_t SMEM_pos = (offset_x + threadIdx_X) + (offset_y + threadIdx_Y) * tileBlockSize;
-					uint32_t GMEM_pos = (offset_x + threadIdx_X) + (offset_y + threadIdx_Y) * K;	// Already confirmed that K = M = N
-					// AS and BS are layed out the same way in memory as they are in global memory
-					AS[SMEM_pos] = A[GMEM_pos];
-					BS[SMEM_pos] = B[GMEM_pos];
-				}
-			}
-			__syncthreads();	// Ensure all data has been loaded into shared memory
-
-			A += tileBlockSize * M;
-			B += tileBlockSize;
+	if constexpr (BLOCKSIZE_M == BLOCKSIZE_N) {
+		__shared__ float AS[tileBlockLength_N * tileBlockLength_K];
+		__shared__ float BS[tileBlockLength_K * tileBlockLength_N];
 
 
+		// Positiion of thread in each blocktile
+		const uint32_t A_threadIdx_X = threadIdx.x % tileBlockLength_M;
+		const uint32_t A_threadIdx_Y = threadIdx.x / tileBlockLength_M;
 
-		}
+		const uint32_t B_threadIdx_X = threadIdx.x % tileBlockLength_K;
+		const uint32_t B_threadIdx_Y = threadIdx.x / tileBlockLength_K;
 	}
 }
