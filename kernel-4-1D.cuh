4e902afb452961ade8747b16200a1a0e2a649e5a
MarekOcz <kitty29@talktalk.net>
2025-08-31 11:43:56 +0100

    Initial commit for kernel 4

---
+++ kernel-4-1D.cuh
@@ -0,0 +1,113 @@
+#pragma once
+#include "hintfile-1D.cuh"
+
+#define BLOCKTILED 1
+
+template <uint32_t M, uint32_t N, uint32_t K,
+	uint32_t BLOCKDIM, uint32_t TILESIZE_M = BLOCKDIM, uint32_t TILESIZE_N = TILESIZE_M,
+	uint32_t TM = 8U, uint32_t TN = TM, memory_location LOAD_INTO = memory_location::REGISTERS>
+__global__ void SGEMM(const float* __restrict A, const float* __restrict B, float* const __restrict C, const float alpha = 1.0f, const float beta = 0.0f) {
+	// This is mostly the same as kernel-3-1D so the majority of code is copy-pasted from that
+		// Square matrices of equal dimensions
+	if constexpr (TILESIZE_M == TILESIZE_N && M == N && TM == TN && LOAD_INTO == memory_location::REGISTERS) {
+
+		// Evaluating constexprs. These are optimised at compile time and don't use up a register
+		constexpr uint32_t TILESIZE_K = TM;
+
+		constexpr uint32_t BLOCKTILE_LENGTH_M = TILESIZE_M * TM;	// Tile size in the M dimension
+		constexpr uint32_t BLOCKTILE_LENGTH_N = TILESIZE_N * TN;	// Tile size in the N dimension
+		constexpr uint32_t BLOCKTILE_LENGTH_K = TILESIZE_K;			// Tile size in the K dimension
+
+		constexpr uint32_t BLOCKTILE_AREA_A = BLOCKTILE_LENGTH_M * BLOCKTILE_LENGTH_K;
+		constexpr uint32_t BLOCKTILE_AREA_B = BLOCKTILE_LENGTH_N * BLOCKTILE_LENGTH_K;
+		constexpr uint32_t BLOCKTILE_AREA_C = BLOCKTILE_LENGTH_M * BLOCKTILE_LENGTH_N;	// Size of the tile block in C
+
+		constexpr uint32_t BLOCKSIZE = BLOCKDIM * BLOCKDIM;
+
+		__shared__ float AS[BLOCKTILE_LENGTH_M * BLOCKTILE_LENGTH_K];
+		__shared__ float BS[BLOCKTILE_LENGTH_K * BLOCKTILE_LENGTH_N];
+
+		const uint32_t threadRow = threadId % TILESIZE_M;
+		const uint32_t threadCol = threadId / TILESIZE_M;
+
+		A += blockIdx_X * BLOCKTILE_LENGTH_M;
+		B += blockIdx_Y * BLOCKTILE_LENGTH_N * K;
+
+		float threadResults[TM * TN] = {};
+
+
+		if constexpr (K % BLOCKTILE_LENGTH_K == 0 && M % BLOCKTILE_LENGTH_M == 0 && BLOCKTILE_AREA_A % BLOCKSIZE == 0 && BLOCKSIZE % BLOCKTILE_LENGTH_M == 0) {
+			float regA[TM];
+			float regB[TM];
+
+			// Position of thread in each blocktile
+			const uint32_t A_threadIdx_X = threadId % BLOCKTILE_LENGTH_M;
+			const uint32_t A_threadIdx_Y = threadId / BLOCKTILE_LENGTH_M;
+
+			const uint32_t B_threadIdx_X = threadId % BLOCKTILE_LENGTH_K;
+			const uint32_t B_threadIdx_Y = threadId / BLOCKTILE_LENGTH_K;
+
+			constexpr uint32_t STRIDE_A = BLOCKSIZE / BLOCKTILE_LENGTH_M;
+			constexpr uint32_t STRIDE_B = BLOCKSIZE / BLOCKTILE_LENGTH_K;
+
+			for (uint32_t k = 0; k < K; k += BLOCKTILE_LENGTH_K) {
+
+				// Loading data into shared memory
+				// Loading A
+				#pragma unroll
+				for (uint32_t A_i = 0; A_i < BLOCKTILE_LENGTH_K; A_i += STRIDE_A) {
+					// 0, 2, 4, 6
+
+					AS[(A_i + A_threadIdx_Y) * BLOCKTILE_LENGTH_M + A_threadIdx_X] = A[(A_i + A_threadIdx_Y) * M + A_threadIdx_X];
+				}
+
+				// Loading B
+				#pragma unroll
+				for (uint32_t B_i = 0; B_i < BLOCKTILE_LENGTH_N; B_i += STRIDE_B) {
+					// B_i = 0, 32, 64, 96
+					BS[(B_i + B_threadIdx_Y) * BLOCKTILE_LENGTH_K + B_threadIdx_X] = B[(B_i + B_threadIdx_Y) * K + B_threadIdx_X];
+				}
+
+				syncThreads(); // Ensure all data has been loaded into SMEM
+
+				A += BLOCKTILE_LENGTH_K * M;
+				B += BLOCKTILE_LENGTH_K;
+
+				for (uint32_t dotIdx = 0; dotIdx < BLOCKTILE_LENGTH_K; ++dotIdx) {
+					// Loading values into registers
+					{	// Limit the scope of A_pos to free a register
+						const uint32_t A_pos = threadRow * TM + BLOCKTILE_LENGTH_M * dotIdx;	// Don't have to compute this value on every iteration
+						#pragma unroll
+						for (uint32_t TM_i = 0; TM_i < TM; ++TM_i) {
+							regA[TM_i] = AS[A_pos + TM_i];
+						}
+					}
+					{	// Limit the scope of B_pos to free a register
+						const uint32_t B_pos = TN * threadCol * BLOCKTILE_LENGTH_K + dotIdx;	// Don't have to compute this value on every iteration of i
+						#pragma unroll
+						for (uint32_t TN_i = 0; TN_i < TN; ++TN_i) {
+							regB[TN_i] = BS[TN_i * BLOCKTILE_LENGTH_K + B_pos];
+						}
+					}
+					for (uint32_t TM_i = 0; TM_i < TM; ++TM_i) {
+						for (uint32_t TN_i = 0; TN_i < TN; ++TN_i) {
+							threadResults[TM_i + TN_i * TM] += regA[TM_i] * regB[TN_i];
+						}
+					}
+				}
+				syncThreads();
+			}
+		}
+		// Writing the results into C
+		if constexpr (LOAD_INTO == memory_location::REGISTERS) {
+			const uint32_t C_Block = blockIdx_X * BLOCKTILE_LENGTH_M + blockIdx_Y * BLOCKTILE_LENGTH_N * M;	// Top left position of the block
+			for (uint32_t TM_i = 0; TM_i < TM; ++TM_i) {
+				for (uint32_t TN_i = 0; TN_i < TN; ++TN_i) {
+					const uint32_t C_pos = C_Block + (threadRow * TM + TM_i) + M * (TN_i + threadCol * TN);
+					C[C_pos] = alpha * threadResults[TM_i + TN_i * TM] + beta * C[C_Block];
+				}
+			}
+		}
+	}
+
+}
