dbaea28f73b5f584a13c14153b056b0942c28df6
Marek-Oczadly <moczadly@outlook.com>
2025-09-03 20:23:35 +0100

    defined constexprs for kernel-5-1D

--- kernel-5-1D.cuh
+++ kernel-5-1D.cuh
@@ -4,25 +4,40 @@
 #define BLOCKTILED 1
 
 
-template <uint32_t M, uint32_t N, uint32_t K, uint32_t BLOCKDIM, uint32_t TILESIZE_M = BLOCKDIM, uint32_t TILESIZE_N = TILESIZE_M, 
-	uint32_t WARP_SUBTILES_M = 2U, uint32_t WARP_SUBTILES_M = 2U, uint32_t WM = 4U, uint32_t WN = 2U, uint32_t TM = 4U, uint32_t TN = TM, 
-	memory_location LOAD_INTO = memory_location::REGISTERS, uint32_t WARPSIZE = 32U>
+template <uint32_t M, uint32_t N, uint32_t K, uint32_t BLOCKDIM, uint32_t M_N_ratio = 2U, 
+		  uint32_t WARP_SUBTILES = 2U, uint32_t WARP_TILES = 2U, uint32_t TM = 4U, uint32_t TN = TM, 
+		  memory_location LOAD_INTO = memory_location::REGISTERS, uint32_t WARPSIZE = 32U>
 __global__ void SGEMM(const float* __restrict A, const float* __restrict B, float* const __restrict C, const float alpha = 1.0f, const float beta = 0.0f) {
 
-	constexpr uint32_t BLOCKTILE_LENGTH_M = TILESIZE_M * WM;
-	constexpr uint32_t BLOCKTILE_LENGTH_N = TILESIZE_N * WN;
-	constexpr uint32_t BLOCKTILE_LENGTH_K = WN * TN;
+	constexpr uint32_t BLOCKTILE_LENGTH_M = BLOCKDIM * M_N_ratio * TM;
+	constexpr uint32_t BLOCKTILE_LENGTH_N = BLOCKDIM * TN;
+	constexpr uint32_t BLOCKTILE_LENGTH_K = TM * WARP_SUBTILES;
+	
+	constexpr uint32_t WARP_TILE_LENGTH_M = BLOCKTILE_LENGTH_M / WARP_TILES;
+	constexpr uint32_t WARP_TILE_LENGTH_N = BLOCKTILE_LENGTH_N / WARP_TILES;
 
-	constexpr uint32_t 
+	constexpr uint32_t WARP_SUBTILE_AREA = WARP_TILE_LENGTH_M * WARP_TILE_LENGTH_N;
 
-	constexpr uint32_t WARP_SUBTILE_LENGTH_M = ;
-	constexpr uint32_t WARP_SUBTILE_LENGTH_M = ;
+	constexpr uint32_t WARP_SUBTILE_LENGTH_M = BLOCKTILE_LENGTH_M / W_SCALE;
+	constexpr uint32_t WARP_SUBTILE_LENGTH_N = BLOCKTILE_LENGTH_N / W_SCALE;
 
 	constexpr uint32_t NUMTHREADS = BLOCKDIM * BLOCKDIM;
+
+	constexpr uint32_t M_OVER_4 = BLOCKTILE_LENGTH_M / 4;
+	constexpr uint32_t K_OVER_4 = BLOCKTILE_LENGTH_K / 4;
 
 	const uint32_t warpId =		  threadId / WARPSIZE;
 	const uint32_t threadInWarp = threadId % WARPSIZE;
 
+	float threadResults[TM * TN * WARP_SUBTILES * WARP_SUBTILES] = {};	// Initialise values to 0
+
+	__shared__ float AS[BLOCKTILE_LENGTH_K * BLOCKTILE_LENGTH_M];
+	__shared__ float BS[BLOCKTILE_LENGTH_K * BLOCKTILE_LENGTH_N];
+
+	A += blockIdx_X * BLOCKTILE_LENGTH_M;
+	B += blockIdx_Y * BLOCKTILE_LENGTH_N * K;
+
+	
 	
 
 
