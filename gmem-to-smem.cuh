50ff0a4e36af9b72c3dcdc5b28df628b5c1c9c79
Marek-Oczadly <moczadly@outlook.com>
2025-12-24 08:29:50 +0000

    Written helper functions

--- gmem-to-smem.cuh
+++ gmem-to-smem.cuh
@@ -6,7 +6,14 @@
 /// @tparam NUM_ELEMENTS The number of elements being loaded from GMEM to SMEM
 template <uint32_t BLOCKSIZE, uint32_t NUM_ELEMENTS>
 __device__ __forceinline void naiveLoadToSMEM(const float* const __restrict& GMEM, float(&SMEM)[NUM_ELEMENTS]) {
-	static_assert(BLOCKSIZE < 1024, "Blocksize must be less than 1024"
+	static_assert(BLOCKSIZE < 1024, "Blocksize must be less than 1024");
+
+	constexpr uint32_t ELEMENTS_PER_THREAD = NUM_ELEMENTS / BLOCKSIZE;
+	constexpr uint32_t LOADS_PER_ITERATION = BLOCKSIZE;
+
+	for (uint32_t i = threadId; i < NUM_ELEMENTS; i += BLOCKSIZE) {
+		SMEM[i] = GMEM[i];
+	}
 }
 
 /// @brief Loads elements from GMEM into SMEM using vectorised loads
@@ -14,7 +21,7 @@ __device__ __forceinline void naiveLoadToSMEM(const float* const __restrict& GME
 /// @tparam NUM_ELEMENTS The size of the SMEM array
 template <uint32_t BLOCKSIZE, uint32_t NUM_ELEMENTS>
 __device__ __forceinline void vectorisedLoadToSMEM(const float* const __restrict& GMEM, float(&SMEM)[NUM_ELEMENTS]) {
-	static_assert(BLOCKSIZE < 1024, "Blocksize must be less than 1024")
+	static_assert(BLOCKSIZE < 1024, "Blocksize must be less than 1024");
 	
 	constexpr uint32_t ELEMENTS_PER_THREAD = NUM_ELEMENTS / BLOCKSIZE;
 	constexpr uint32_t NUM_VECTORISED_LOAD = ELEMENTS_PER_THREAD / 4U;
@@ -28,5 +35,16 @@ __device__ __forceinline void vectorisedLoadToSMEM(const float* const __restrict
 /// @brief Loads elements from GMEM to SMEM using swizzled and vectorised loads
 template <uint32_t BLOCKSIZE, uint32_t NUM_ELEMENTS>
 __device__ __forceinline void swizzledLoadToSMEM(const float* const __restrict& GMEM, float(&SMEM)[NUM_ELEMENTS]) {
-	static_assert(BLOCKSIZE < 1024, "Blocksize must be less than 1024"
+	static_assert(BLOCKSIZE < 1024, "Blocksize must be less than 1024");
+
+	constexpr uint32_t SWIZZLE_SIZE = 
+
+}
+
+template <uint32_t BLOCKSIZE, uint32_t NUM_ELEMENTS, uint32_t INTERVAL_BETWEEN_PADDING>
+__device__ __forceinline void paddedLoadToSMEM(const float* const __restrict& GMEM, float(&SMEM)[arrSizeWithPadding(NUM_ELEMENTS, INTERVAL_BETWEEN_PADDING)]) {
+	static_assert(BLOCKSIZE < 1024, "Blocksize must be less than 1024");
+	
+	constexpr uint32_t OUTPUT_ARR_SIZE = arrSizeWithPadding(NUM_ELEMENTS, INTERVAL_BETWEEN_PADDING);
+
 }
--- hintfile-microprofiling.cuh
+++ hintfile-microprofiling.cuh
@@ -3,3 +3,12 @@
 #include <cstdint>
 
 #define threadId threadIdx.x
+#define CONSTEVAL_STATIC_ASSERT(c, msg) do { if (!(c)) throw msg; } while(false)
+
+
+
+consteval uint32_t arrSizeWithPadding(uint32_t arr_size, uint32_t interval_between_padding) {
+	CONSTEVAL_STATIC_ASSERT((arr_size % interval_between_padding == 0), "The parameter interval_between_padding must be a fact of arr_size");
+	const uint32_t num_rows = arr_size / interval_between_padding;
+	return num_rows * (interval_between_padding + 1);
+}
